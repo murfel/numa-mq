a1b224c188a6aa749b9a1003fa96c1f9665cf466
murfel <murfel@users.noreply.github.com>
2021-03-18 11:57:54 +0300

    renaming members in numa_dummy_high_throughput_counter

--- numa_dummy_high_throughput_counter.h
+++ numa_dummy_high_throughput_counter.h
@@ -7,23 +7,23 @@
 class numa_dummy_high_throughput_counter {
 private:
     const int num_nodes;
-    std::vector<two_choice_counter *> multicounters;
+    std::vector<two_choice_counter *> node_counters;
 
-    two_choice_counter & node_multicounter(int node_id) {
-        return *multicounters[node_id];
+    two_choice_counter & get_node_counter(int node_id) {
+        return *node_counters[node_id];
     }
 public:
-    explicit numa_dummy_high_throughput_counter(int num_nodes, std::size_t num_counters_on_each_node) : num_nodes(num_nodes) {
+    numa_dummy_high_throughput_counter(int num_nodes, std::size_t num_counters_on_each_node) : num_nodes(num_nodes) {
         for (int i = 0; i < num_nodes; i++) {
             auto data = numa_alloc_onnode(sizeof(two_choice_counter), i);
-            multicounters.push_back(new(data) two_choice_counter(num_counters_on_each_node, i));
+            node_counters.push_back(new(data) two_choice_counter(num_counters_on_each_node, i));
         }
     }
     void add(int node_id, int thread_id) {
-        node_multicounter(node_id).add(thread_id);
+        get_node_counter(node_id).add(thread_id);
     }
     uint64_t get(int node_id, int thread_id) {
-        return num_nodes * node_multicounter(node_id).get(thread_id);
+        return num_nodes * get_node_counter(node_id).get(thread_id);
     }
 };
 
