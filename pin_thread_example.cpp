c08cd2d877f48b18b0d0de6feecf8e2d0f24aa2f
murfel <murfel@users.noreply.github.com>
2021-03-27 21:27:12 +0300

    pin thread example: assert on thread id

--- pin_thread_example.cpp
+++ pin_thread_example.cpp
@@ -1,13 +1,13 @@
 #include <vector>
 #include <thread>
-#include <mutex>
 #include <sched.h>
 #include <numa.h>
 
 #include <boost/thread/barrier.hpp>
 
-void thread_routine(boost::barrier & barrier) {
+void thread_routine(boost::barrier & barrier, int thread_id) {
     barrier.wait();
+    assert(sched_getcpu() == thread_id);
     assert(sched_getcpu() / 18 == numa_node_of_cpu(sched_getcpu()));
 }
 
@@ -24,7 +24,7 @@ int main() {
     std::vector<std::thread> threads;
     boost::barrier barrier(num_threads + 1);
     for (std::size_t i = 0; i < num_threads; i++) {
-        threads.emplace_back(thread_routine, std::ref(barrier));
+        threads.emplace_back(thread_routine, std::ref(barrier), i);
         pin_thread(i, threads.back());
     }
     barrier.wait();
