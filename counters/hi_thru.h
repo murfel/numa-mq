7d760a85b7acfe0205bdec7836393b9114a0caff
murfel <murfel@users.noreply.github.com>
2021-03-27 21:07:34 +0300

    fix hi thru counter--ignore num_counters completely, use num threads on one node instead

--- counters/hi_thru.h
+++ counters/hi_thru.h
@@ -27,7 +27,7 @@ private:
 public:
     hi_thru(std::size_t num_counters, int node_id) : num_counters(num_counters) {
         counters = (aligned_non_atomic_counter *) numa_alloc_onnode(sizeof(aligned_non_atomic_counter) * threads_on_node, node_id);
-        for (std::size_t i = 0; i < threads_on_node; i++) {
+        for (int i = 0; i < threads_on_node; i++) {
             new(&counters[i]) aligned_non_atomic_counter;
         }
     }
@@ -35,7 +35,7 @@ public:
         get_counter(thread_id)++;
     }
     uint32_t get(int thread_id) {
-        return num_counters * get_counter(thread_id);
+        return threads_on_node * get_counter(thread_id);
     }
 };
 
